6f1b379de91b44261155f580134142a019090bf1
Miguel Ferreira <meikibay@gmail.com>
2018-05-25 12:29:11 +0200

    Added Menu Editing Section Into the Admin Panel of Wordpress

--- functions.php
+++ functions.php
@@ -1,6 +1,12 @@
 <?php
 /* Adds  the function that allows the admin to set a featured image on a post*/
 
-add_theme_support( 'post-thumbnails' ); 
+add_theme_support( 'post-thumbnails' );
+
+/*Adding Menu Editing section to the Admin Panel*/
+function register_my_menu() {
+  register_nav_menu('header-menu',__( 'Header Menu' ));
+}
+add_action( 'init', 'register_my_menu' );
 
 ?>
