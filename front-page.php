4171d9e7d29fb1dcda728ccab8b23dd752a55fe5
Miguel Ferreira <meikibay@gmail.com>
2018-05-29 18:27:46 +0200

    Removed unused content and added the correct buttons formatting

--- front-page.php
+++ front-page.php
@@ -1,31 +1,4 @@
 <?php get_header();?>
-<!--Content Overlaping the Hero Image with Map, Adress and Schedule
-			<a target="_blank" href="<?php wp_get_attachment_url()?> schedule-2" class="button is-light is-medium">
-				Our Schedule
-			</a>
-
-
-		<video autoplay="true" muted="true" loop="true" controls="true" type="video/mp4" src="https://www.youtube.com/watch?v=z2RYB8MFY2o&feature=youtu.be"></video>
-		<section "container is-fullhd">
-			<div class="columns">
-				<div class="column is-half">
-						Sample Text
-					<div>
-				<div class="column"><img src="<?php echo get_template_directory_uri(); ?>/img/.png" id="maplocation">
-				</div>
-			</div>
-		</section>
-	<section "container is-fullhd">
-		<div class="columns">
-			<div class="column is-half">
-		    	<img src="<?php echo get_template_directory_uri(); ?>/img/pablo.jpg">
-			<div class="column">
-				Sample text 2
-			</div>
-		</div>
-		</div>
-	</section>
-	-->
 
 	<section class="hero is-large">
 		<video autoplay muted loop type="video/mp4" src="<?php echo get_template_directory_uri(); ?>/video/rental.mp4"></video>
@@ -35,29 +8,29 @@
 	<section class="3col">
 		<div class="columns">
 			<div class="column is-half has-text-centered">
-				<h2>Who we are</h2>
-				<a class="button is-danger">Read more about me</a>
+				<h2 class="title marg"> Equipment Available</h2>
+				<a class="button is-danger">Rent what you need here!</a>
 			</div>
 			<div class="column is-half">
-				<img src="http://via.placeholder.com/350x200" alt="" class ="tri">
+				<img src="<?php echo get_template_directory_uri(); ?>/img/studio.jpg" alt="" class ="tri">
 			</div>
 		</div>
 		<div class="columns is-centered">
 			<div class="column is-half">
-				<img src="http://via.placeholder.com/350x200" alt="" class ="tri">
+				<img src="<?php echo get_template_directory_uri(); ?>/img/pablo.jpg" alt="" class ="tri">
 			</div>
 			<div class="column is-half has-text-centered">
-				<h3>Who am I</h3>
-					<a class="button is-danger">about this</a>
+				<h3 class="title marg">Props and Puppets? We can make them!</h3>
+					<a class="button is-danger">Check them out here!</a>
 			</div>
 		</div>
 		<div class="columns is-centered">
 			<div class="column is-half has-text-centered">
-				<h3>Does this work</h3>
-					<a class="button is-danger">about another thing</a>
+				<h3 class="title marg">We are creators!</h3>
+					<a class="button is-danger">Have a Request?</a>
 			</div>
 			<div class="column is-half">
-				<img src="http://via.placeholder.com/350x200" alt="" class ="tri">
+				<img src="<?php echo get_template_directory_uri(); ?>/img/makers.jpg" alt="" class ="tri">
 			</div>
 		</div>
 	</section>
@@ -77,33 +50,25 @@
 	  </div>
 	</section>
 
-	<section class="">
+	<section>
 		<div class="columns">
-			<div class="column is-half">
-				<img src="http://via.placeholder.com/350x200" alt="" class ="tri">
+			<div class="column is-half has-text-centered container">
+				<a href="#">
+					<img src="<?php echo get_template_directory_uri(); ?>/img/who.png" alt="" class ="image duo">
+					<div class="overlay">
+						<div class="text">Who We Are</div>
+					</div>
+				</a>
 			</div>
-			<div class="column is-half has-text-centered">
-				<h2>Who we are</h2>
-				<a class="button is-danger">Read more about me</a>
-			</div>
+			<div class="column is-half has-text-centered container">
+				<a href="#">
+					<img src="<?php echo get_template_directory_uri(); ?>/img/what.png" alt="" class ="image duo">
+					<div class="overlay">
+						<div class="text">What We Do</div>
+					</div>
+				</a>
 		</div>
 	</section>
 
-
-
-
-<!--JavaScript for the slider (hiding & displaying next image)
-	<script type="text/javascript">
-		var slides = document.querySelectorAll('#slides .slide');
-		var currentSlide = 0;
-		var slideInterval = setInterval(nextSlide,2500);
-
-		function nextSlide(){
-		slides[currentSlide].className = 'slide';
-		currentSlide = (currentSlide+1)%slides.length;
-		slides[currentSlide].className = 'slide showing';
-		}
-	</script>
--->
 
 <?php get_footer(); ?>
