a21ff23b28d0e0a305898d95db34b2bcf34b9d48
Miguel Ferreira <meikibay@gmail.com>
2018-05-29 18:44:04 +0200

    Adjustes Size of the Thumbnail and moved the title upwards

--- single.php
+++ single.php
@@ -1,15 +1,14 @@
 <?php get_header();?>
 
 <section class="content has-text-centered full-hd">
-	<?php the_post_thumbnail('full'); ?>
-	<br>
 	<h1><?php the_title();?></h1>
-
+	<?php the_post_thumbnail('medium'); ?>
+	<br>
+	<br>
 	<?php
 	the_post();
 	the_content();
 	?>
-
 </section>
 
 <?php get_footer(); ?>
