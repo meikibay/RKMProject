079d089f436757f0810174dc5faa596ffb5ec4b6
Miguel Ferreira <meikibay@gmail.com>
2018-05-29 18:29:41 +0200

    Edited page for post content

--- single.php
+++ single.php
@@ -1,13 +1,15 @@
-<?php get_header('special');?>
+<?php get_header();?>
 
 <section class="content has-text-centered full-hd">
+	<?php the_post_thumbnail('full'); ?>
+	<br>
 	<h1><?php the_title();?></h1>
 
-	<?php 
+	<?php
 	the_post();
 	the_content();
 	?>
-	
+
 </section>
 
 <?php get_footer(); ?>
