5ab27bbd0c84db6c9354fd66e69a35670ce4ed5f
Miguel Ferreira <meikibay@gmail.com>
2018-05-29 18:32:41 +0200

    Created blog page for wordpress

---
+++ blog.php
@@ -0,0 +1,34 @@
+<?php get_header();?>
+<?php
+    // the post loop
+    if ( have_posts() ) {
+        while ( have_posts() ) {
+
+            the_post();
+            // Post Content here
+            ?>
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+                        <div class="post-preview">
+                             <a href ="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
+                                <h2 class="post-title">
+                                    <?php the_title();?>
+                                </h2>
+                            </a>
+                            <h4 class="post-subtitle">
+                                <?php the_excerpt(); ?>
+                            </h4>
+                            <p class="post-meta">This thing was posted by
+                                <?php the_author(); ?> at <?php the_time(); ?>
+                            </p>
+                        </div>
+                    <hr>
+                    </div>
+                </div>
+            </div>
+        <?php
+        } // end while
+    } // end if
+?>
+<?php get_footer();?>
